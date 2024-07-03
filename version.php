941e69736dab08897bf146be8fdd023ba0852019
Emir Kamel <p0092396@brookes.ac.uk>
2024-07-03 14:03:50 +0100

    defining service functions and version bump

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024060401;
+$plugin->version = 2024070301;
 $plugin->requires = 2012120301;
 $plugin->component = 'local_obu_metalinking';
 $plugin->maturity = MATURITY_STABLE;
