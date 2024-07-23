f565189d119ae579dce9492edd33a5678df7f41f
Joe Souch <p0091841@brookes.ac.uk>
2024-07-23 14:44:29 +0100

    Update version

--- version.php
+++ version.php
@@ -23,12 +23,12 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024070302;
+$plugin->version = 2024072201;
 $plugin->requires = 2012120301;
 $plugin->component = 'local_obu_metalinking';
 $plugin->maturity = MATURITY_STABLE;
 
-$plugin->release = 'v2.0.0';
+$plugin->release = 'v2.0.1';
 $plugin->dependencies = array(
     'enrol_meta' => 2022112800,
     'local_obu_group_manager' => 2024060401
